2bbfa43d730668dfecfa8093a2833e9994588e3f
Thais Hamilton <thaismha@gmail.com>
2015-07-13 03:35:21 -0300

    Adds remove task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 var ViewModel = function(taskName){
   this.tasks = ko.observableArray(["Js: Learn Knockout","Book: Chapt 01 Code Complete", "Fun: Dragon Ball Super - E02"]);
   this.task = ko.observable(taskName);
+  this.selectedTasks = ko.observableArray(); 
+
+  this.addTask = function(){
+    this.tasks.push(this.task());
+    this.task("");
+  };
+
+  this.removeTask = function(){
+    this.tasks.removeAll(this.selectedTasks());
+  };
 };
 
 ko.applyBindings(new ViewModel(""));
