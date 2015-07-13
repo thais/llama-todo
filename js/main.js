3de924d69edc65f458384c17c3b2a058125101e5
Thais Hamilton <thaismha@gmail.com>
2015-07-13 02:15:37 -0300

    Shows the hardcoded list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var ViewModel = function(taskName){
- this.task = ko.observable(taskName);
+  this.tasks = ko.observableArray(["Js: Learn Knockout","Book: Chapt 01 Code Complete", "Fun: Dragon Ball Super - E02"]);
+  this.task = ko.observable(taskName);
 };
 
 ko.applyBindings(new ViewModel(""));
